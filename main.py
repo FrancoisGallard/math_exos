b03bb466489d4712566d805a77ce436251308d61
Francois Gallard <francois.gallard@irt-saintexupery.com>
2025-01-18 22:39:14 +0100

    Version working with latexttable

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # This is a sample Python script.
-
+import latextable
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import sympy as sp
 from sympy import Symbol, expand, factor, rootof
 from random import randrange
 import tabulate
+from texttable import  Texttable
+from sympy.polys.polyerrors import GeneratorsNeeded
 
 import random
 
@@ -24,51 +26,83 @@ def expand_poly_apbsq(min_coeff=-12, max_coeff=12):
     roots = [rootof(fact, 0), rootof(fact, 1)]
     return exp, fact, roots
 
+def latexify_table(lines, headers):
+    table_quest = Texttable()
+    align=["p{5cm}","p{9cm}"]
+    if len(headers)==3:
+        align.append("p{3cm}")
+    table_quest.set_cols_align(align)
+    table_quest.add_rows([headers] + lines)
+
+    latex_quest = latextable.draw_latex(table_quest)
+    latex_quest=latex_quest.replace(r"\begin{table}","")
+    latex_quest=latex_quest.replace(r"\end{table}", "")
+
+    return latex_quest
+
+
+def pretty_print_eq(eq):
+    return "$"+str(eq).replace("**","^").replace("*","")+"$"
 def generate_table(gen_func, n_expr, add_roots=True):
-    table_sol = []
-    table_question=[]
+    lines_sol = []
+    lines_question=[]
+
     for i in range(n_expr):
         exp, fact, roots = gen_func()
-        line=[str(exp), str(fact)]
+        line=[pretty_print_eq(exp), pretty_print_eq(fact)]
         if add_roots:
-            line.append( str(roots))
-        table_sol.append(line)
-        table_question.append([str(exp), "                  ", "                   "])
+            line.append( pretty_print_eq(roots))
+        lines_sol.append(line)
+        lines_question.append([pretty_print_eq(exp), "                  ", "                   "])
     headers=["Factoriser", "Solution" ]
     if add_roots:
         headers.append("Racines")
-    latex_sol=tabulate.tabulate(table_sol, headers=headers, tablefmt="latex_longtable",)
-    latex_quest = tabulate.tabulate(table_question, headers=headers, tablefmt="latex_longtable")
+
+    latex_sol=latexify_table( lines_sol, headers)
+    latex_quest=latexify_table(lines_question, headers)
     return latex_sol, latex_quest
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    latex_1sol,latex_1_quest= generate_table(expand_poly_aminbsq, 10, add_roots=True)
-    latex_2sol, latex_2_quest = generate_table(expand_poly_apbsq, 10, add_roots=True)
-    solution= open("solution.tex",'w')
-    questions=open("questions.tex",'w')
+def generate_latex_files(latex_1sol,latex_1_quest, latex_2sol, latex_2_quest):
+    solution = open("solution.tex", 'w')
+    questions = open("questions.tex", 'w')
     try:
         for outf in [solution, questions]:
 
-            outf.write(r"\documentclass[11pt,twoside,a4paper]{article}"+"\n")
-            outf.write(r"\usepackage{booktabs,siunitx}" + "\n")
+            outf.write(r"\documentclass[11pt,a4paper]{article}"+"\n")
+            #outf.write(r"\usepackage{booktabs,siunitx}" + "\n")
+            outf.write(r"\usepackage[margin=1cm, tmargin=0cm, textheight=27cm]{geometry}" + "\n")
+
             outf.write(r"\begin{document}"+"\n")
+            outf.write(r"\date{}" + "\n")
+
             outf.write(r"\title{Entrainement aux expressions remarquables}"+"\n")
             outf.write(r"\maketitle"+"\n")
+
+            outf.write(r"{\renewcommand{\arraystretch}{2}"+"\n")
             if outf==solution:
                 outf.write(latex_1sol)
-                outf.write(  "\n\n")
+                outf.write(  "\n")
                 outf.write(latex_2sol)
             else:
                 outf.write(latex_1_quest)
-                outf.write("\n\n")
+                outf.write("\n")
                 outf.write(latex_2_quest)
+
+            outf.write("}\n")
             outf.write(r"\end{document}"+"\n")
     finally:
         solution.close()
         questions.close()
 
 
+if __name__ == '__main__':
+    err=True
+    while err:
+        try:
+            latex_1sol,latex_1_quest= generate_table(expand_poly_aminbsq, 10, add_roots=True)
+            latex_2sol, latex_2_quest = generate_table(expand_poly_apbsq, 10, add_roots=True)
+            err=False
+        except GeneratorsNeeded:
+            err=True
+    generate_latex_files(latex_1sol, latex_1_quest, latex_2sol, latex_2_quest)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
